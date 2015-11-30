66d79d18c162a09a851be7360043786dc9ac3409
nilpanescoll <nil.panes@gmail.com>
2015-11-30 10:13:11 +0100

    AngularJS app

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angello2App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angello2App
+ */
+angular.module('angello2App')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angello2App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angello2App
+ */
+angular.module('angello2App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
